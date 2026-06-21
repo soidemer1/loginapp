a7b6288ed22a466721dfd4786bad577fa0e792af
ANTHONY JESÚS REMEDIOS DELGADO <66584418@ceibal.edu.uy>
2026-06-21 15:59:40 -0300

    Conectar MySQL en la nube

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "QQmen2JC4g",
-    database: "loginapp"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
